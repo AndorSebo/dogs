6f040b0d9a9a3972a623d3da9aeee9616fc9267c
AndorSebo <andorsebo@gmail.com>
2020-01-08 13:57:13 +0100

    DAOJSON kiegészítve jackson librarykkal (ObjectMapper működik)

--- dao-json/src/main/java/DAOJSON.java
+++ dao-json/src/main/java/DAOJSON.java
@@ -1,4 +1,6 @@
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import dao.DogDAO;
 import dogs.exceptions.AgeInvalidException;
 import dogs.exceptions.MissingDog;
@@ -6,25 +8,50 @@ import dogs.exceptions.MovingIsTooLate;
 import dogs.model.Dog;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.UUID;
 
-public class DAOJSON  implements DogDAO {
+public class DAOJSON implements DogDAO {
 
     File jsonFile;
     ObjectMapper mapper;
 
     public DAOJSON(File jsonFile) {
         this.jsonFile = jsonFile;
+        ctorExtension();
     }
 
     public DAOJSON(String jsonPath) {
-        
+        this.jsonFile = new File(jsonPath);
+        ctorExtension();
+    }
+
+    private void ctorExtension() {
+        if (!jsonFile.exists())
+            try {
+                jsonFile.createNewFile();
+                FileWriter writer = new FileWriter(jsonFile);
+                writer.write("[]");
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT);
     }
 
     @Override
     public void createDog(Dog dog) throws AgeInvalidException, MovingIsTooLate {
-
+        Collection<Dog> dogs = listAllDogs();
+        dogs.add(dog);
+        try{
+            mapper.writeValue(jsonFile,dogs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
